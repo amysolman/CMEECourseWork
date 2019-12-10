679fde1ff447cd1aa311c1417300cc4847a15457
Amy Solman <amy.solman19@imperial.ac.uk>
2019-12-10 10:58:48 +0000

    HPC coursework added.

--- Week9/Code/asolman_HPC_2019_main.R
+++ Week9/Code/asolman_HPC_2019_main.R
@@ -338,7 +338,137 @@ cluster_run <- function(speciation_rate, size, wall_time, interval_rich, interva
 # Question 20 
 process_cluster_results <- function()  {
   # clear any existing graphs and plot your graph within the R window
-  combined_results <- list() #create your list output here to return
+  rm(list=ls())
+  graphics.off()
+  
+  
+  octave_500 <- list()
+  octave_1000 <- list()
+  octave_2500 <- list()
+  octave_5000 <- list()
+  
+  
+  ##########LOAD SPECIES OCTAVES AND RICHNESS BY SIMULATION SIZE###########
+  for (i in 1:100) {
+    loaded <- load(paste0("simulation_",i,".rda"))
+    if (size == 500) {
+      octave_500[[i]] <- species_octaves_list
+    } else {
+      if (size == 1000) {
+        octave_1000[[i]] <- species_octaves_list
+      } else {
+        if (size == 2500) {
+          octave_2500[[i]] <- species_octaves_list
+        } else {
+          if (size == 5000) {
+            octave_5000[[i]] <- species_octaves_list
+          }
+        }
+      }
+    }
+    
+  }
+  
+  ##########REMOVE NULL VALUES#########
+  
+  l1 <- lapply(octave_1000, function(x) x[lengths(x) > 0])
+  octave_1000 <- l1[lengths(l1) >  0]
+  
+  l1 <- lapply(octave_2500, function(x) x[lengths(x) > 0])
+  octave_2500 <- l1[lengths(l1) >  0]
+  
+  l1 <- lapply(octave_500, function(x) x[lengths(x) > 0])
+  octave_500 <- l1[lengths(l1) >  0]
+  
+  l1 <- lapply(octave_5000, function(x) x[lengths(x) > 0])
+  octave_5000 <- l1[lengths(l1) >  0]
+  
+  #For octave_500 we need to work out the average species abundance octaves AFTER 4000 generations
+  
+  #for simulation 1-25 in octave_5000 we want to save only results after 4000 generations (column 1)
+  
+  #removes species abundance octaves before burn in time
+  for (i in 1:25) {
+    octave_500[[i]][1:80] <- NULL
+    octave_1000[[i]][1:80] <- NULL
+    octave_2500[[i]][1:80] <- NULL
+    octave_5000[[i]][1:80] <- NULL
+  }
+  
+  ###use octave_500, octave_1000, octave_2500 and octave_5000 to create four bar charts of average species abundance 
+  
+  i <- 1 #assign value of 1 to i
+  x <- octave_500[[1]][i]
+  y <- octave_500[[1]][i+1]
+  x <- sum_vect(x[[1]], y[[1]])
+  
+  for (i in 2:25) {
+    y <- octave_500[[1]][i+1]
+    x <- sum_vect(x[[1]], y[[1]])
+    
+  }
+  
+  oct_500_total <- x/(length(octave_500)) #divide the totalled octave values by the number of octave values
+  
+  i <- 1 #assign value of 1 to i
+  x <- octave_1000[[1]][i]
+  y <- octave_1000[[1]][i+1]
+  x <- sum_vect(x[[1]], y[[1]])
+  
+  for (i in 2:25) {
+    y <- octave_1000[[1]][i+1]
+    x <- sum_vect(x[[1]], y[[1]])
+    
+  }
+  
+  oct_1000_total <- x/(length(octave_1000)) 
+  
+  i <- 1 #assign value of 1 to i
+  x <- octave_2500[[1]][i]
+  y <- octave_2500[[1]][i+1]
+  x <- sum_vect(x[[1]], y[[1]])
+  
+  for (i in 2:25) {
+    y <- octave_2500[[1]][i+1]
+    x <- sum_vect(x[[1]], y[[1]])
+    
+  }
+  
+  oct_2500_total <- x/(length(octave_2500)) 
+  
+  i <- 1 #assign value of 1 to i
+  x <- octave_5000[[1]][i]
+  y <- octave_5000[[1]][i+1]
+  x <- sum_vect(x[[1]], y[[1]])
+  
+  for (i in 2:25) {
+    y <- octave_5000[[1]][i+1]
+    x <- sum_vect(x[[1]], y[[1]])
+    
+  }
+  
+  oct_5000_total <- x/(length(octave_5000)) 
+  
+  
+  graphics.off() 
+  
+  par(mfcol=c(2,2)) #initialize multi-paneled plot
+  par(mfg = c(1,1)) #specify which subplot to use first
+  barplot(oct_500_total,
+          xlab = "octaves", ylab = "Count", col = "blue", border = "pink",
+          main = 'Community Size 500') #Add title
+  par(mfg = c(1,2)) #Second sub-plot
+  barplot(oct_1000_total, xlab = "octaves", ylab = "Count", col = "green", 
+          border = "pink", main = 'Community Size 1000')
+  par(mfg = c(2,1)) #Second sub-plot
+  barplot(oct_2500_total, xlab = "octaves", ylab = "Count", col = "red", 
+          border = "pink", main = 'Community Size 2500')
+  par(mfg = c(2,2)) #Second sub-plot
+  barplot(oct_5000_total, xlab = "octaves", ylab = "Count", col = "yellow", 
+          border = "pink", main = 'Community Size 5000')
+  
+  
+  combined_results <- list(oct_500_total, oct_1000_total, oct_2500_total, oct_5000_total) #create your list output here to return
   return(combined_results)
 }
 
--- Week9/Code/asolman_HPC_2019_test.R
+++ Week9/Code/asolman_HPC_2019_test.R
@@ -1,50 +1,34 @@
-# CMEE 2019 HPC excercises R code HPC run code proforma
+# clear any existing graphs and plot your graph within the R window
+rm(list=ls())
+graphics.off()
 
-rm(list=ls()) # good practice 
-source("asolman_HPC_2019_main.R")
-
-# Question 20 
-process_cluster_results <- function()  {
-  # clear any existing graphs and plot your graph within the R window
-  rm(list=ls())
-  graphics.off()
-  
 
 octave_500 <- list()
-richness_500 <- list()
 octave_1000 <- list()
-richness_1000 <- list()
 octave_2500 <- list()
-richness_2500 <- list()
 octave_5000 <- list()
-richness_5000 <- list()
 
 
 ##########LOAD SPECIES OCTAVES AND RICHNESS BY SIMULATION SIZE###########
-  for (i in 1:20) {
-    loaded <- load(paste0("../Code/PracticeSims/simulation_",i,".rda"))
-    if (size == 500) {
+for (i in 1:100) {
+  loaded <- load(paste0("simulation_",i,".rda"))
+  if (size == 500) {
     octave_500[[i]] <- species_octaves_list
-    richness_500[[i]] <- species_richness_list
-    
+  } else {
+    if (size == 1000) {
+      octave_1000[[i]] <- species_octaves_list
     } else {
-      if (size == 1000) {
-        octave_1000[[i]] <- species_octaves_list
-        richness_1000[[i]] <- species_richness_list
+      if (size == 2500) {
+        octave_2500[[i]] <- species_octaves_list
       } else {
-        if (size == 2500) {
-          octave_2500[[i]] <- species_octaves_list
-          richness_2500[[i]] <- species_richness_list
-        } else {
-          if (size == 5000) {
-            octave_5000[[i]] <- species_octaves_list
-            richness_5000[[i]] <- species_richness_list
-          }
+        if (size == 5000) {
+          octave_5000[[i]] <- species_octaves_list
         }
       }
     }
-    
   }
+  
+}
 
 ##########REMOVE NULL VALUES#########
 
@@ -60,140 +44,90 @@ octave_500 <- l1[lengths(l1) >  0]
 l1 <- lapply(octave_5000, function(x) x[lengths(x) > 0])
 octave_5000 <- l1[lengths(l1) >  0]
 
-l1 <- lapply(richness_1000, function(x) x[lengths(x) > 0])
-richness_1000 <- l1[lengths(l1) >  0]
+#For octave_500 we need to work out the average species abundance octaves AFTER 4000 generations
 
-l1 <- lapply(richness_2500, function(x) x[lengths(x) > 0])
-richness_2500 <- l1[lengths(l1) >  0]
+#for simulation 1-25 in octave_5000 we want to save only results after 4000 generations (column 1)
 
-l1 <- lapply(richness_500, function(x) x[lengths(x) > 0])
-richness_500 <- l1[lengths(l1) >  0]
+#removes species abundance octaves before burn in time
+for (i in 1:25) {
+  octave_500[[i]][1:80] <- NULL
+  octave_1000[[i]][1:80] <- NULL
+  octave_2500[[i]][1:80] <- NULL
+  octave_5000[[i]][1:80] <- NULL
+}
 
-l1 <- lapply(richness_5000, function(x) x[lengths(x) > 0])
-richness_5000 <- l1[lengths(l1) >  0]
-  
-    
-  
-  # #this loads the files as a list
-  # rda2list <- function(file) {
-  #   e <- new.env()
-  #   load(file, envir = e)
-  #   as.list(e)
-  # }
-  # 
-  # folder <- "../Code/PracticeSims"
-  # files <- list.files(folder, pattern = ".rda$")
-  # 
-  # simulations <- Map(rda2list, file.path(folder, files))
-  # names(simulations) <- tools::file_path_sans_ext(files)
-  
-  
-  #calculate mean species abundance octaves for each simulation size AFTER burn time
-  
-  #Functing should also return a list of four vectors corresponding to the octave outputs
-  #that plot the four bar graphs - the vectors should appear in the list in increasing size order
-  #e.g. size = 500 first
-  #Hint: use the load function on your .rda files and use sum_vect
-  
-  #Four bar graphs in a multipanel graph (one for each simulation size)
-  #each showing mean species abundance octave result for simulation runs of that size
-  #Only use data of abundance octave after the burn time is up
-  
-  par(mfcol=c(2,2)) #initialize multi-paneled plot
-  par(mfg = c(1,1)) #specify which subplot to use first
-  barplot(oct_500,
-          xlab = "octaves", ylab = "Count", col = "lightblue", border = "pink",
-          main = 'Run Size 500')
-  par(mfg = c(1,2)) #Second subplot
-  barplot(oct_1000, 
-          xlab = "octaves", ylab = "Count", col = "lightgreen", border = "pink", 
-          main = 'Run Size 1000')
-  par(mfg = c(2,1)) #third subplot
-  barplot(oct_2500,
-          xlab = "octaves", ylab = "Count", col = "lightblue", border = "pink",
-          main = 'Run Size 2500')
-  par(mfg = c(2,2)) #fourth subplot
-  barplot(oct_5000,
-          xlab = "octaves", ylab = "Count", col = "lightblue", border = "pink",
-          main = 'Run Size 5000')
-  
-  combined_results <- list() #create your list output here to return
-  return(combined_results)
-  
-  
-  graphics.off()
-  plot(1, type="n", xlab="", ylab="", xlim=c(0, 4), ylim=c(0, 4), cex=0.2)
-  
-  
-  
-  
-  # Question 24
-  #Create a function turtle in R to draw a line of a given length from a given point (defined as a vector)
-  #and in a given direction. So turtle will have three inputs: start_position, direction (measured in radians, 
-  #not degrees) and length. As well as drawing the line, turtle should return the end point of the line
-  #it just drew as a vector. Turtle should not open the plot, it should just draw the line on an already open plot.
-  #(Hint: you need to use sin and cos).
-  
-  radians <- degrees * pi / 180 # applies 'degrees' multiplied by pie divided by 180 to radians
-  #there are two radians in 360 degrees
-  
-  #first let's open a plot to test on:
-  plot(1, type="n", xlab="", ylab="", xlim=c(-4, 4), ylim=c(-4, 4), cex=0.2)
-  
-  turtle(start_position = c(1,1), direction = 0.5, length = 2)
-  
-  turtle <- function(start_position, direction, length)  {
-    #start position plus lenght plus direction = new point
-    x0 <- start_position[1]
-    y0 <- start_position[2]
-    points(x0,y0)
-    
-    x1 <- length*cos(direction) + x0
-    y1 <- length*sin(direction) + y0
-    points(x1,y1)
-    endpoint <- c(x1,y1)
-    #draw a line from position 1 to position 2
-    segments(x0, y0, x1 = x1, y1 = y1, col = 'blue')
+###use octave_500, octave_1000, octave_2500 and octave_5000 to create four bar charts of average species abundance 
 
-    return(endpoint) # you should return your endpoint here.
-  }
-  
-  #Now create another function elbow that calls turtle twice to draw a pair 
-  #of lines that join together with a given angle between them. As with turtle, 
-  #it should not open a new plot and should assume a suitable plot is already open.
-  #The function elbow should accept as an input: the starting point, direction 
-  #and length of the first line. The second point should start at the end point of the first line, 
-  #have a direction that is 45 degrees (pi*/4 radians) to the right of that first line and have 
-  #a length that is 0.95 times the length of the first line
-  
-  plot(1, type="n", xlab="", ylab="", xlim=c(-2, 8), ylim=c(0, 8), cex=0.2)
-  
+i <- 1 #assign value of 1 to i
+x <- octave_500[[1]][i]
+y <- octave_500[[1]][i+1]
+x <- sum_vect(x[[1]], y[[1]])
 
+for (i in 2:25) {
+  y <- octave_500[[1]][i+1]
+  x <- sum_vect(x[[1]], y[[1]])
   
-  spiral(start_position = c(1,1), direction = 0.5, length = 2)
-  
-  
-  start_position = c(1,1)
-  direction = 0.5
-  length = 2
+}
 
-  
-  
-  spiral <- function(start_position, direction, length)  {
-    
-    while (length > 0.1) {
-      
-      first_line <- turtle(start_position, direction, length)
-      
-      second_line <-spiral(start_position = first_line, direction = direction + (pi/4), length = length*0.95)
-    }
-      
-    return("This function uses turtle to draw the first line, then calls itself the draw proceeding lines from the last point, with direction angle + new angle, with gradually decreasing length.") #get return to work
-  } 
-    
-  
-  
-  
+oct_500_total <- x/(length(octave_500)) #divide the totalled octave values by the number of octave values
 
-  
+i <- 1 #assign value of 1 to i
+x <- octave_1000[[1]][i]
+y <- octave_1000[[1]][i+1]
+x <- sum_vect(x[[1]], y[[1]])
 
+for (i in 2:25) {
+  y <- octave_1000[[1]][i+1]
+  x <- sum_vect(x[[1]], y[[1]])
+  
+}
+
+oct_1000_total <- x/(length(octave_1000)) 
+
+i <- 1 #assign value of 1 to i
+x <- octave_2500[[1]][i]
+y <- octave_2500[[1]][i+1]
+x <- sum_vect(x[[1]], y[[1]])
+
+for (i in 2:25) {
+  y <- octave_2500[[1]][i+1]
+  x <- sum_vect(x[[1]], y[[1]])
+  
+}
+
+oct_2500_total <- x/(length(octave_2500)) 
+
+i <- 1 #assign value of 1 to i
+x <- octave_5000[[1]][i]
+y <- octave_5000[[1]][i+1]
+x <- sum_vect(x[[1]], y[[1]])
+
+for (i in 2:25) {
+  y <- octave_5000[[1]][i+1]
+  x <- sum_vect(x[[1]], y[[1]])
+  
+}
+
+oct_5000_total <- x/(length(octave_5000)) 
+
+
+graphics.off() 
+
+par(mfcol=c(2,2)) #initialize multi-paneled plot
+par(mfg = c(1,1)) #specify which subplot to use first
+barplot(oct_500_total,
+        xlab = "octaves", ylab = "Count", col = "blue", border = "pink",
+        main = 'Community Size 500') #Add title
+par(mfg = c(1,2)) #Second sub-plot
+barplot(oct_1000_total, xlab = "octaves", ylab = "Count", col = "green", 
+        border = "pink", main = 'Community Size 1000')
+par(mfg = c(2,1)) #Second sub-plot
+barplot(oct_2500_total, xlab = "octaves", ylab = "Count", col = "red", 
+        border = "pink", main = 'Community Size 2500')
+par(mfg = c(2,2)) #Second sub-plot
+barplot(oct_5000_total, xlab = "octaves", ylab = "Count", col = "yellow", 
+        border = "pink", main = 'Community Size 5000')
+
+
+combined_results <- list(oct_500_total, oct_1000_total, oct_2500_total, oct_5000_total) #create your list output here to return
+return(combined_results)
